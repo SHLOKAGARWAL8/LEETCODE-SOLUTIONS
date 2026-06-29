4a4e650a1427ec00949ebd1a3ae363437f0b64a4
SHLOKAGARWAL8 <shlokagarwal64@gmail.com>
2026-06-29 19:46:52 +0530

    Time: 5 ms (79.54%) | Memory: 46.2 MB (6.74%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x<0 || (x%10==0 && x!=0))
+            return false;
+        int copy=x,d=0,s=0;
+        while(copy>0)
+        {
+            d=copy%10;
+            s=s*10+d;
+            copy/=10;
+        }
+        return s==x;
+    }
+}
