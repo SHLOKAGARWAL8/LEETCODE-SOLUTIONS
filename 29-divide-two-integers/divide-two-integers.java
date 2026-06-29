436da72c4dd4c5570a4e9cb576a96f26a6114787
SHLOKAGARWAL8 <shlokagarwal64@gmail.com>
2026-06-29 19:55:45 +0530

    Time: 1 ms (93.41%) | Memory: 42.8 MB (31.05%) - LeetSync

---
+++ 29-divide-two-integers/divide-two-integers.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int divide(int dividend, int divisor) {
+        int result=dividend/divisor;
+        if(dividend==-2147483648 && divisor==-1)
+            return Integer.MAX_VALUE;
+        return result;
+    }
+}
